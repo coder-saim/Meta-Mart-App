e7105aed6e25d2ab2a293c508a35004a84c366b0
coder-saim <saim.cse.du.744@gmail.com>
2022-09-04 03:46:56 +0600

    All activity related to sing in/up/out are added. Firebase authentication added Fragmentation added Bottom navigation View added

--- app/src/main/java/com/example/votenow/MainActivity.java
+++ app/src/main/java/com/example/votenow/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.votenow;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button exploreButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        exploreButton = findViewById(R.id.exploreButton);
+
+        exploreButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,SignInActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
