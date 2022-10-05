a3c92019e547d6faab3258b52bf0d21a89389e96
coder-saim <saim.cse.du.744@gmail.com>
2022-10-06 01:23:46 +0600

    Splash Screen added......

--- app/src/main/java/com/example/votenow/activities/MainActivity.java
+++ app/src/main/java/com/example/votenow/activities/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 
@@ -13,7 +14,6 @@ import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button exploreButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,14 +22,16 @@ public class MainActivity extends AppCompatActivity {
 
         Objects.requireNonNull(getSupportActionBar()).hide();
 
-        exploreButton = findViewById(R.id.exploreButton);
+        //Splash Screen added......
 
-        exploreButton.setOnClickListener(new View.OnClickListener() {
+        new Handler().postDelayed(new Runnable() {
             @Override
-            public void onClick(View view) {
+            public void run() {
                 Intent intent = new Intent(MainActivity.this, SignInActivity.class);
                 startActivity(intent);
             }
-        });
+        },1000);
+
+
     }
 }
